4f99205923c8ab26e8c23c613792c028bf228ae3
Carlos Jorge Pereira <144349074+podefteza@users.noreply.github.com>
2024-09-24 13:04:03 +0000

    202409241404

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: carlos-j <carlos-j@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 14:57:18 by carlos-j          #+#    #+#             */
-/*   Updated: 2024/09/23 12:20:26 by carlos-j         ###   ########.fr       */
+/*   Updated: 2024/09/24 13:02:24 by carlos-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,40 +29,64 @@ unlink, wait, waitpid*/
 //		^no permissions: <filename>: Permission denied
 // 		^no cmd: error (command not found: <cmd>)
 
+int	file_check(char *filename, int is_input)
+{
+	int	fd;
+
+	if (is_input)
+	{
+		fd = open(filename, O_RDONLY);
+		if (fd == -1)
+		{
+			perror(filename);
+			return (-1);
+		}
+	}
+	else
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd == -1)
+		{
+			perror(filename);
+			return (-1);
+		}
+	}
+	return (fd);
+}
+
+int	cmds(int argc, char **argv)
+{
+	char	**cmds_input;
+	char	**cmds_output;
+
+	cmds_input = ft_split(argv[1], " ");
+
+
+
+
+
+}
+
 int	main(int argc, char **argv)
 {
-	int infile_fd, outfile_fd;
+	int	infile_fd;
+	int	outfile_fd;
+
 	if (argc != 5)
 	{
 		ft_putstr_fd("Usage: ./pipex infile cmd1 cmd2 outfile\n", 2);
 		return (1);
 	}
-	if (access(argv[1], F_OK) != 0)
-	{
-		ft_putstr_fd(argv[1], 2);
-		ft_putstr_fd(": No such file or directory\n", 2);
-		return (1);
-	}
-	if (access(argv[1], R_OK) != 0)
-	{
-		ft_putstr_fd(argv[1], 2);
-		ft_putstr_fd(": Permission denied\n", 2);
-		return (1);
-	}
-	infile_fd = open(argv[1], O_RDONLY);
+	infile_fd = file_check(argv[1], 1);
 	if (infile_fd == -1)
-	{
-		perror(argv[1]);
 		return (1);
-	}
-	outfile_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	outfile_fd = file_check(argv[4], 0);
 	if (outfile_fd == -1)
 	{
-		ft_putstr_fd(argv[4], 2);
-		ft_putstr_fd(": Permission denied\n", 2);
 		close(infile_fd);
 		return (1);
 	}
+	// pipex
 	close(infile_fd);
 	close(outfile_fd);
 	return (0);
--- pipex.h
+++ pipex.h
@@ -1,27 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carlos-j <carlos-j@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 13:05:02 by carlos-j          #+#    #+#             */
+/*   Updated: 2024/09/23 13:10:15 by carlos-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <unistd.h>
-//For most Unix-based system calls (file operations, process control, pipes, duplication).
-#include <fcntl.h>
-//For open().
-#include <stdlib.h>
-//For malloc() and free().
-#include <stdio.h>
-//For perror().
-#include <string.h>
-//For strerror().
-#include <sys/wait.h>
-//For wait() and waitpid().
-
-
+# include <unistd.h>
+// For most Unix-based system calls (file operations, process control, pipes, duplication).
+# include <fcntl.h>
+// For open().
+# include <stdlib.h>
+// For malloc() and free().
+# include <stdio.h>
+// For perror().
+# include <string.h>
+// For strerror().
+# include <sys/wait.h>
+// For wait() and waitpid().
 
 // libft
 void	ft_putstr_fd(char *s, int fd);
 size_t	ft_strlen(const char *s);
-
-
-
-
 
 #endif
